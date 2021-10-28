c16187adec4b39712be6c343a378f30dea340918
roach <tmdgusya@naver.com>
2021-10-28 21:17:13 +0900

    test

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
 
     public static void main(String[] args) {
-
+        Settings settings = Settings.getInstance();
+        Settings settings1 = Settings.getInstance();
+        System.out.println(settings == settings1);
     }
 
 }
