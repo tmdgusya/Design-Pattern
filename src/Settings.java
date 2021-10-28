6782701a4ff4ac0f51072f4fa800f7dbf1036657
roach <tmdgusya@naver.com>
2021-10-28 21:07:48 +0900

    외부에서 같은 인스턴스를 사용할 수 있도록 제공

--- src/Settings.java
+++ src/Settings.java
@@ -1,15 +1,21 @@
 public class Settings {
 
+    private static Settings instance;
+
     /**
      * Class 밖에서 new 를 사용할 수 없도록 제어
      */
     private Settings() {}
 
     /**
+     * 외부에서 같은 인스턴스를 이용하도록 if 문 조건 추가
      * @return 외부에서 사용할 수 있는 인스턴스 제공
      */
     public static Settings getInstance() {
-        return new Settings();
+        if (instance == null) {
+            instance = new Settings();
+        }
+        return instance;
     }
 
 }
