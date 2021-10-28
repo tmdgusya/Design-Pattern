7a7cf41bcfdeaa1bab524eb898fd41319a129c88
roach <tmdgusya@naver.com>
2021-10-28 21:33:10 +0900

    static inner class

--- src/Settings.java
+++ src/Settings.java
@@ -1,25 +1,20 @@
 public class Settings {
 
-    private static volatile Settings instance;
-
     /**
      * Class 밖에서 new 를 사용할 수 없도록 제어
      */
     private Settings() {}
 
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
     /**
-     * Double Checked Locking
+     * Thread-Safe
      * @return 외부에서 사용할 수 있는 인스턴스 제공
      */
     public static Settings getInstance() {
-        if (instance == null) {
-            synchronized (Settings.class) {
-                if (instance == null) {
-                    instance = new Settings();
-                }
-            }
-        }
-        return instance;
+        return SettingsHolder.INSTANCE;
     }
 
 }
