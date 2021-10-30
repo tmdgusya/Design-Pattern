8649d7e3bfbe0e5de6d2d017efeb9f1a233ec1be
roach <tmdgusya@naver.com>
2021-10-30 20:34:35 +0900

    OCP를 준수하는 코드로 변경

--- src/abstract_factory/WhiteShipFactory.java
+++ src/abstract_factory/WhiteShipFactory.java
@@ -2,11 +2,17 @@ package abstract_factory;
 
 public class WhiteShipFactory implements ShipFactory {
 
+    private ShipPartsFactory shipPartsFactory;
+
+    public WhiteShipFactory(ShipPartsFactory shipPartsFactory) {
+        this.shipPartsFactory = shipPartsFactory;
+    }
+
     @Override
     public Ship createShip() {
         Ship ship = new WhiteShip();
-        ship.setAnchor(new WhiteAnchor());
-        ship.setWheel(new WhiteWheel());
+        ship.setAnchor(shipPartsFactory.createAnchor());
+        ship.setWheel(shipPartsFactory.createWheel());
         return ship;
     }
 
